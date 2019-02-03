11037ad083bb43bcba00ea40a131a5eacbbbb2bd
Jan Deinhard <jan.deinhard@gmail.com>
2019-02-03 21:54:06 +0100

    WIP: add continuation to client interface

--- example/client.cpp
+++ example/client.cpp
@@ -1,3 +1,5 @@
+#include "../include/remote/detail/then.h"
+
 #include "../include/remote/rpc_client.h"
 
 #include <boost/asio.hpp>
@@ -10,31 +12,39 @@
 
 int main() {
     try {
+        std::mutex io_mutex;
+
         boost::asio::io_service io_service;
+
         rpc_client c{io_service, "127.0.0.1", 12345};
 
-        std::future<std::tuple<int, std::string>> foo_result = c.async_call<int, std::string>("foo", 123);
-        std::future<std::tuple<double>> sin_result = c.async_call<double>("sin", 3.14);
+        std::cout << "on main thread " << std::this_thread::get_id() << std::endl;
 
-        std::thread t([&io_service]() {
-            io_service.run();
+        std::future<std::tuple<int, std::string>> foo_result = c.async_call<int, std::string>("foo", 123);
+        const auto t1 = detail::then(foo_result, [&io_mutex](std::future<std::tuple<int, std::string>>& res) {
+            std::lock_guard<std::mutex> guard{io_mutex};
+            std::cout << "on t1 thread " << std::this_thread::get_id() << std::endl;
+            try {
+                const auto tuple = res.get();
+                std::cout << "Result: " << std::get<0>(tuple) << ", " << std::get<1>(tuple) << std::endl;
+            } catch (const std::exception& ex) {
+                std::cout << "Failure: " << ex.what() << std::endl;
+            }
         });
 
-        try {
-            auto f = foo_result.get();
-            std::cout << "Result: " << std::get<0>(f) << ", " << std::get<1>(f) << std::endl;
-        } catch (const std::exception& ex) {
-            std::cout << "Failure: " << ex.what() << std::endl;
-        }
+        std::future<std::tuple<double>> sin_result = c.async_call<double>("sin", 3.14);
+        const auto t2 = detail::then(sin_result, [&io_mutex](std::future<std::tuple<double>>& res) {
+            std::lock_guard<std::mutex> guard{io_mutex};
+            std::cout << "on t2 thread " << std::this_thread::get_id() << std::endl;
+            try {
+                auto tuple = res.get();
+                std::cout << "Result: " << std::get<0>(tuple) << std::endl;
+            } catch (const std::exception& ex) {
+                std::cout << "Failure: " << ex.what() << std::endl;
+            }
+        });
 
-        try {
-            auto f = sin_result.get();
-            std::cout << "Result: " << std::get<0>(f) << std::endl;
-        } catch (const std::exception& ex) {
-            std::cout << "Failure: " << ex.what() << std::endl;
-        }
-
-        t.join();
+        io_service.run();
 
     } catch (const std::exception& ex) {
         std::cout << ex.what() << std::endl;
