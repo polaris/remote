e5bcf3909dc578e67118bbf805e7056e07acc57c
Jan Deinhard <jan.deinhard@gmail.com>
2019-02-05 11:17:27 +0100

    no mutex required anymore

--- example/client.cpp
+++ example/client.cpp
@@ -12,18 +12,15 @@
 
 int main() {
     try {
-        std::mutex io_mutex;
-
         boost::asio::io_service io_service;
 
         rpc_client c{io_service, "127.0.0.1", 12345};
 
-        std::cout << "on main thread " << std::this_thread::get_id() << std::endl;
+        std::cout << "On thread: " << std::this_thread::get_id() << std::endl;
 
         rpc_future<int, std::string> foo_result = c.async_call<int, std::string>("foo", 123);
-        auto t1 = foo_result.then([&io_mutex](std::future<std::tuple<int, std::string>>& res) {
-            std::lock_guard<std::mutex> guard{io_mutex};
-            std::cout << "on t1 thread " << std::this_thread::get_id() << std::endl;
+        auto t1 = foo_result.then([](std::future<std::tuple<int, std::string>>& res) {
+            std::cout << "On thread: " << std::this_thread::get_id() << std::endl;
             try {
                 const auto tuple = res.get();
                 std::cout << "Result: " << std::get<0>(tuple) << ", " << std::get<1>(tuple) << std::endl;
@@ -33,9 +30,8 @@ int main() {
         });
 
         rpc_future<double> sin_result = c.async_call<double>("sin", 3.14);
-        auto t2 = sin_result.then([&io_mutex](std::future<std::tuple<double>>& res) {
-            std::lock_guard<std::mutex> guard{io_mutex};
-            std::cout << "on t2 thread " << std::this_thread::get_id() << std::endl;
+        auto t2 = sin_result.then([](std::future<std::tuple<double>>& res) {
+            std::cout << "On thread: " << std::this_thread::get_id() << std::endl;
             try {
                 auto tuple = res.get();
                 std::cout << "Result: " << std::get<0>(tuple) << std::endl;
