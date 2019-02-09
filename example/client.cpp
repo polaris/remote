19e0d113b65cc2133ddd32d6048be84d1f8be309
Jan Deinhard <jan.deinhard@gmail.com>
2019-02-09 11:55:33 +0100

    remove obsolete output

--- example/client.cpp
+++ example/client.cpp
@@ -16,11 +16,8 @@ int main() {
 
         remote::rpc_client c{io_service, "127.0.0.1", 12345};
 
-        std::cout << "On thread: " << std::this_thread::get_id() << std::endl;
-
         remote::rpc_future<int, std::string> foo_result = c.async_call<int, std::string>("foo", 123);
         auto t1 = foo_result.then([](std::future<std::tuple<int, std::string>>& res) {
-            std::cout << "On thread: " << std::this_thread::get_id() << std::endl;
             try {
                 const auto tuple = res.get();
                 std::cout << "Result: " << std::get<0>(tuple) << ", " << std::get<1>(tuple) << std::endl;
@@ -31,9 +28,8 @@ int main() {
 
         remote::rpc_future<double> sin_result = c.async_call<double>("sin", 3.14);
         auto t2 = sin_result.then([](std::future<std::tuple<double>>& res) {
-            std::cout << "On thread: " << std::this_thread::get_id() << std::endl;
             try {
-                auto tuple = res.get();
+                const auto tuple = res.get();
                 std::cout << "Result: " << std::get<0>(tuple) << std::endl;
             } catch (const std::exception& ex) {
                 std::cout << "Failure: " << ex.what() << std::endl;
