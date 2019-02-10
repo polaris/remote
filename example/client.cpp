30e549c525e1d43a1e57596f7a989d2fb20a510a
Jan Deinhard <jan.deinhard@gmail.com>
2019-02-10 10:20:56 +0100

    guard output with a mutex

--- example/client.cpp
+++ example/client.cpp
@@ -16,9 +16,12 @@ int main() {
 
         remote::rpc_client c{io_service, "127.0.0.1", 12345};
 
+        std::mutex mutex;
+
         remote::rpc_future<int, std::string> foo_result = c.async_call<int, std::string>("foo", 123);
-        auto t1 = foo_result.then([](std::future<std::tuple<int, std::string>>& res) {
+        auto t1 = foo_result.then([&mutex](std::future<std::tuple<int, std::string>>& res) {
             try {
+                std::lock_guard<std::mutex> lock{mutex};
                 const auto tuple = res.get();
                 std::cout << "Result: " << std::get<0>(tuple) << ", " << std::get<1>(tuple) << std::endl;
             } catch (const std::exception& ex) {
@@ -27,8 +30,9 @@ int main() {
         });
 
         remote::rpc_future<double> sin_result = c.async_call<double>("sin", 3.14);
-        auto t2 = sin_result.then([](std::future<std::tuple<double>>& res) {
+        auto t2 = sin_result.then([&mutex](std::future<std::tuple<double>>& res) {
             try {
+                std::lock_guard<std::mutex> lock{mutex};
                 const auto tuple = res.get();
                 std::cout << "Result: " << std::get<0>(tuple) << std::endl;
             } catch (const std::exception& ex) {
